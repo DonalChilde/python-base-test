c95e41e044ffc88eb13323b2cf17dec6716fdc76
Chad Lowe <pfmsoft.dev@gmail.com>
2023-01-08 05:09:40 -0700

    bump version to 0.0.1

--- src/python_base_test/__init__.py
+++ src/python_base_test/__init__.py
@@ -3,6 +3,6 @@
 __author__ = "Chad Lowe"
 __email__ = "pfmsoft.dev@gmail.com"
 # The short X.Y.Z version.
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 # The full version, including alpha/beta/rc tags.
 __release__ = __version__
